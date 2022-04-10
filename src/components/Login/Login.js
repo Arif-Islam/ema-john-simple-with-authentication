eec66e69e2823951ab9a4a6745d82267a7478afe
Arif <iariful950@gmail.com>
2022-04-11 01:58:09 +0600

    implemented login and redirected to shop

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,10 +1,67 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { Link, useNavigate } from 'react-router-dom';
+import auth from '../../firebase.init';
 import './Login.css';
 
+
 const Login = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const navigate = useNavigate();
+    const [
+        signInWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useSignInWithEmailAndPassword(auth);
+
+    const handleEmailBlur = event => {
+        setEmail(event.target.value);
+    }
+    const handlePasswordBlur = event => {
+        setPassword(event.target.value);
+    }
+    if (user) {
+        navigate('/shop');
+    }
+    const handleUserSignIn = event => {
+        event.preventDefault();
+        signInWithEmailAndPassword(email, password);
+    }
     return (
-        <div>
-            <h2>This is login</h2>
+        <div className='form-container'>
+            <div>
+                <h2 className='form-title'>Login</h2>
+                <form onSubmit={handleUserSignIn}>
+                    <div className="input-group">
+                        <label htmlFor="email">Email</label>
+                        <input onBlur={handleEmailBlur} type="email" name="email" id="" required />
+                    </div>
+                    <div className="input-group">
+                        <label htmlFor="password">Password</label>
+                        <input onBlur={handlePasswordBlur} type="password" name="password" id="" required />
+                    </div>
+                    <p style={{ color: 'red' }}>{error?.message}</p>
+                    {
+                        loading && <p>Loading...</p>
+                    }
+                    <input className='form-submit' type="submit" value="Login" />
+                </form>
+                <p>
+                    New to Ema-John? <Link className='form-link' to='/signup'>Create an account</Link>
+                </p>
+                <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'flex-end', margin: '15px 15px', color: 'rgba(149, 160, 167, 1)' }}>
+                    <div style={{ width: '42%' }}>
+                        <hr />
+                    </div>
+                    <div style={{ width: '16%', textAlign: 'center' }}>or</div>
+                    <div style={{ width: '42%' }}>
+                        <hr />
+                    </div>
+                </div>
+
+            </div>
         </div>
     );
 };
--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,10 +1,76 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import './SignUp.css';
+import auth from '../../firebase.init';
 
 const SignUp = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setconfirmPassword] = useState('');
+    const [error, setError] = useState('');
+    const navigate = useNavigate();
+    const [createUserWithEmailAndPassword, user] = useCreateUserWithEmailAndPassword(auth);
+
+    const handleEmailBlur = event => {
+        setEmail(event.target.value);
+    }
+    const handlePasswordBlur = event => {
+        setPassword(event.target.value);
+    }
+    const handleConfirmPasswordBlur = event => {
+        setconfirmPassword(event.target.value);
+    }
+    if (user) {
+        navigate('/shop');
+    }
+    const handleCreateUser = event => {
+        event.preventDefault();
+        if (password !== confirmPassword) {
+            setError("Your two passwords didn't match!");
+            return;
+        }
+        if (password.length < 6) {
+            setError("Password must be 6 characters or longer!");
+            return;
+        }
+        createUserWithEmailAndPassword(email, password);
+    }
+
     return (
-        <div>
-            <h2>This is sign up</h2>
+        <div className='form-container'>
+            <div>
+                <h2 className='form-title'>Sign Up</h2>
+                <form onSubmit={handleCreateUser}>
+                    <div className="input-group">
+                        <label htmlFor="email">Email</label>
+                        <input onBlur={handleEmailBlur} type="email" name="email" id="" required />
+                    </div>
+                    <div className="input-group">
+                        <label htmlFor="password">Password</label>
+                        <input onBlur={handlePasswordBlur} type="password" name="password" id="" required />
+                    </div>
+                    <div className="input-group">
+                        <label htmlFor="confirm-password">Confirm Password</label>
+                        <input onBlur={handleConfirmPasswordBlur} type="password" name="confirm-password" id="" required />
+                    </div>
+                    <p style={{ color: 'red' }}>{error}</p>
+                    <input className='form-submit' type="submit" value="Sign Up" />
+                </form>
+                <p>
+                    Already have an account? <Link className='form-link' to='/login'>Login</Link>
+                </p>
+                <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'flex-end', margin: '15px 15px', color: 'rgba(149, 160, 167, 1)' }}>
+                    <div style={{ width: '42%' }}>
+                        <hr />
+                    </div>
+                    <div style={{ width: '16%', textAlign: 'center' }}>or</div>
+                    <div style={{ width: '42%' }}>
+                        <hr />
+                    </div>
+                </div>
+
+            </div>
         </div>
     );
 };
