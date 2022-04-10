e1727fedb4cdc87f360b012b95a98dd0bf2b031b
Arif <iariful950@gmail.com>
2022-04-11 03:06:42 +0600

    updated the shipping form

--- src/__auth_steps.js
+++ src/__auth_steps.js
@@ -9,7 +9,8 @@
 8. useCreateUserWithEmailAndPassword from react-firebase-hooks
 9. if user is created, redirect to the expected page
 10. useSignInWithEmailAndPassword for sign in
-11. 
+11. create RequireAuth component ==> check if user exists, also check user location
+12. in route wrap protected component by using require auth component 
 
 */
 
